4687df9ae9a8931ed694e6a8a3fa9ac77c244d38
DESKTOP-9S37B6N\OMISTAJA <jonikarjalainen25@gmail.com>
2024-07-05 11:24:21 +0300

    Time series second Y axis for humidity. Changed the colors of lines. Go figure

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
-import plotly.express as px
+# import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import dash_bootstrap_components as dbc
 
@@ -16,22 +17,49 @@ df = pd.read_csv('temperature.csv', delimiter=';')
 
 df['Date'] = pd.to_datetime(df['Date'])
 
+# Create a time series plot with two y-axes
+fig = go.Figure()
 
-# Melt the dataframe to long format
-df_long = pd.melt(df, id_vars=['Date'], value_vars=['Temperature', 'Moisture'], 
-                  var_name='Variable', value_name='Value')
+# Add Temperature trace
+fig.add_trace(go.Scatter(
+    x=df['Date'], y=df['Temperature'],
+    name='Temperature',
+    yaxis='y1',
+    line=dict(color='red')
+))
 
+# Add Humidity trace with a second y-axis
+fig.add_trace(go.Scatter(
+    x=df['Date'], y=df['Moisture'],
+    name='Humidity',
+    yaxis='y2',
+    line=dict(color='blue')
+))
 
-# Create a time series plot with both Temperature and Moisture
-fig = px.line(df_long, x='Date', y='Value', color='Variable', title='Temperature and Moisture Over Time')
+# Update layout for second y-axis
+fig.update_layout(
+    title='Temperature and Humidity Over Time',
+    xaxis=dict(title='Date'),
+    yaxis=dict(
+        title='Temperature',
+        titlefont=dict(color='red'),
+        tickfont=dict(color='red')
+    ),
+    yaxis2=dict(
+        title='Humidity',
+        titlefont=dict(color='blue'),
+        tickfont=dict(color='blue'),
+        overlaying='y',
+        side='right'
+    ),
+    legend=dict(x=0, y=1.2),
+    template='plotly_white'
+)
 
 
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H1("Weather Dashboard"), className="mb-2")
-    ]),
-    dbc.Row([
-        dbc.Col(html.H2("Temperature"), className="mb-2")
     ]),
      dbc.Row([
         dbc.Col(dcc.Graph(id='time-series-graph', figure=fig), className="mb-4")
